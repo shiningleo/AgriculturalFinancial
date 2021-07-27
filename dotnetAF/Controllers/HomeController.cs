3e78d9178dc2841eabb8d5f822edb99a98ee4b09
liying <leo_bjcp@yahoo.com>
2021-07-27 11:45:25 +0800

    new pro

---
+++ dotnetAF/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace dotnetHelloWorld.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult nongchang()
+        {
+             ViewData["tuiguang"] = "97";
+             ViewData["licheng"] = "中级酒庄";
+             ViewData["tongshu"] = "5000";
+            return View();
+        }
+        
+    }
+}
---
+++ dotnetAF/Controllers/nongchang.cshtml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace jingying.Pages
+{
+    public class nongchangModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+        public static string tuiguang {
+            get;
+
+            set;
+        }
+
+
+        public static string licheng
+        {
+            get;
+
+            set;
+        }
+        public static string tongshu
+        {
+            get;
+
+            set;
+        
+        }
+    }
+}
